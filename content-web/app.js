1a3e92648da2862577b87805b82fb3366c58d6a8
christopher ilizarbe <christophermen@gmail.com>
2021-11-30 22:04:29 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("9c598f5e-0566-4e63-9803-3cfa33a7a919");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://20.124.96.15:3001";
 
